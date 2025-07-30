231d835a71d7f9e27fbf4fb067d6795160e086a8
Candice Bell <candicebell.learninghub@gmail.com>
2025-07-30 12:28:57 -0400

    Adding temporary changes to Github

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-
-# Register your models here.
+# Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # CarModelInline class
 
@@ -11,5 +12,3 @@ from .models import CarMake, CarModel
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
-admin.site.register(CarMake)
-admin.site.register(CarModel)
